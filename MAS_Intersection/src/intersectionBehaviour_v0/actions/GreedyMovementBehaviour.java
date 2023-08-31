9e8ec986a67f2d73541b9d639ac1ace5306b43ed
ZathSL <simone.lissandrello@edu.unito.it>
2023-08-31 18:36:08 +0200

    I've implemented doDelete on TerminationAgentBehaviour

--- MAS_Intersection/src/intersectionBehaviour_v0/actions/GreedyMovementBehaviour.java
+++ MAS_Intersection/src/intersectionBehaviour_v0/actions/GreedyMovementBehaviour.java
@@ -69,7 +69,7 @@ public class GreedyMovementBehaviour extends CyclicBehaviour {
                     myAgent.send(messageRequest);
                     //Attendo due secondi per dare il tempo agli altri agenti di inviarmi le risposte
                     try {
-                        Thread.sleep(2000);
+                        Thread.sleep(5000);
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
